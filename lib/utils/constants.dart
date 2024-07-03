d5a5204051d029ba5d7bf468444997b05c0fd816
guiima <rafael_guiimaraes@hotmail.com>
2024-07-02 23:09:53 -0300

    incluindo o then ao adicionar um produto

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,4 +1,4 @@
 class Constants {
   static const productBaseUrl =
-      'https://shop-cod3r-c70a4-default-rtdb.firebaseio.com';
+      'https://accenture-test-4a071-default-rtdb.firebaseio.com';
 }
