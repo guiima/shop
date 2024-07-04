0253064875cc5ed3c28c318481870d2ebd96db2a
guiima <rafael_guiimaraes@hotmail.com>
2024-07-03 22:35:53 -0300

    buscando os produtos do firebase e printando eles no initialize de products_overview_page

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,4 +1,4 @@
 class Constants {
   static const productBaseUrl =
-      'https://accenture-test-4a071-default-rtdb.firebaseio.com';
+      'https://accenture-test-4a071-default-rtdb.firebaseio.com/products.json';
 }
