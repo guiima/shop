d48b98869cb48d34afd00fe02efefdf5fc6786e6
guiima <rafael_guiimaraes@hotmail.com>
2024-07-08 15:56:41 -0300

    salvando os pedidos no firebase

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,4 +1,6 @@
 class Constants {
   static const productBaseUrl =
       'https://accenture-test-4a071-default-rtdb.firebaseio.com/products';
+  static const orderBaseUrl =
+      'https://accenture-test-4a071-default-rtdb.firebaseio.com/orders';
 }
